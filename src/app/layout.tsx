54027eafd0695ced6cd105d4eff8143539d8f452
A-F-V <faracea2000@gmail.com>
2025-09-30 20:08:52 +0100

    Attempted to fix notch

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import "./globals.css";
 import {
     EB_Garamond,
@@ -45,6 +45,13 @@ export const metadata: Metadata = {
     icons: {
         icon: "/profile.jpg",
     },
+};
+
+export const viewport: Viewport = {
+    width: "device-width",
+    initialScale: 1,
+    maximumScale: 1,
+    viewportFit: "cover",
 };
 
 type RootLayoutProps = {
